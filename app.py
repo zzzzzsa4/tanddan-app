099da5d0c0ac3ef7f4b29d9a8a46d709b420fc4c
zzzzzsa4 <zzzzzsa4@gmail.com>
2026-09-20 21:37:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,18 +78,8 @@ with tab1:
 
   with st.form("new_campaign_form", clear_on_submit=True):
     company = st.text_input("업체명", placeholder="예: 강남 고기집 / 역삼 식당")
-    # 주요 체험단 플랫폼 및 마지막 기타 체험단 포함
     platform = st.selectbox(
-        "플랫폼 선택",
-        [
-            "레뷰",
-            "리뷰노트",
-            "강남맛집",
-            "디너의여왕",
-            "포블로그",
-            "놀러와체험단",
-            "기타 체험단",
-        ],
+        "플랫폼 선택", ["강남맛집", "디너의여왕", "뷰스타", "기타 체험단"]
     )
 
     col1, col2 = st.columns(2)
@@ -101,7 +91,6 @@ with tab1:
     content = st.text_area(
         "제공내역", placeholder="예: 3만원 식사권 또는 제품 협찬"
     )
-    # 진행 상태는 신청중, 선정됨 2개로 유지
     status = st.selectbox("진행 상태", ["신청중", "선정됨"])
 
     submitted = st.form_submit_button("🦔 딴딴이 리스트에 추가하기")
@@ -169,7 +158,6 @@ with tab1:
             st.rerun()
 
 with tab2:
-  st.subheader("🏆 완료된 체험단 아카이브")
   c.execute(
       "SELECT id, company, platform, visit_date, deadline, content, status,"
       " completed_date FROM completed_data"
